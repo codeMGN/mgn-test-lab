38e7400b0e819de5d6031b0b4399b00f85a2ed39
MGN <code@mgn.tech>
2023-03-31 09:43:12 -0600

    fix: remove header and footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,7 @@
-import Header from './Header'
-import Footer from './Footer'
-
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen justify-between leading-relaxed font-Inter">
-      <Header />
       <main className="mb-auto w-full">{children}</main>
-      <Footer />
     </div>
   )
 }
